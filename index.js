49aac326df969bea90c3c19446c1885f8b3a13ee
Ashley Marku <ashley.marku@lc.cuny.edu>
2018-01-12 14:29:36 -0500

    updated

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ this.play = function() {
 
 }
 
-this.pause = function(){
-		song.pause();
-		this.isPlaying = false;
+// this.pauseCurrentSong = function(){
+// 		song.pause();
+// 		this.isPlaying = false;
 
-}
+// }
 
-this.stopCurrentSong = function() {
-	song.pause();
-	this.isPlaying = false;
-	song.currentTime = 0;
-}
+// this.stopCurrentSong = function() {
+// 	song.pause();
+// 	this.isPlaying = false;
+// 	song.currentTime = 0;
+// }
 
 	
 
@@ -31,19 +31,43 @@ this.stopCurrentSong = function() {
 
 var jukebox1=new jukeboxC();
 
-var song = new Audio ("audio/SW.mp3");
+var song1 = new Audio ("audio/HP.mp3");
+var song2 = new Audio ("audio/SW.mp3");
+var song3 = new Audio ("audio/HP.mp3");
+
+var songList = [];
+songList.push(song1,song2,song3);
+
+var	currentSongIndex = 0;
+var song = songList[currentSongIndex];
+
+
 
 function playSong(){
 	song.play();
 
 }
 
+function pauseSong() {
+	song.pause();
+}
+
 function stopSong() {
 	song.pause();
-	song.stopCurrentSong(); // need to make this function
+	song.currentTime = 0;
+	// song.stopCurrentSong(); // need to make this function
 }
 
 function nextSong(){
-	song.next();
+	song.pause();
+	if (currentSongIndex === songList.length-1 ){
+		currentSongIndex = 0;
+	}else {
+		currentSongIndex +=1;
+		console.log(currentSongIndex);
+	}
+	song = new Audio("audio/" + song[currentSongIndex] + ".mp3" );
+	console.log(song[currentSongIndex]);
+	song.play();
+	this.isPlaying = true;
 }
-
